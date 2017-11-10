2c3023754772976e7889831b2b12d775df88cadc
blackcash <jht325@hotmail.com>
2017-11-10 11:15:02 -0600

    Update settings.py

--- lotter/settings.py
+++ lotter/settings.py
@@ -66,6 +66,7 @@ ROBOTSTXT_OBEY = True
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'lotter.pipelines.LotterPipeline': 300,
+    'lotter.pipelines.CounterPipeline': 800,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
